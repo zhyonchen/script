8f00b657c044f3625e433156bea80aba98c79d0f
keyonchen <keyonchern@gmail.com>
2021-08-04 22:10:23 +0800

    1.3

--- judgecount.user.js
+++ judgecount.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         审判计数器
 // @namespace    https://greasyfork.org/zh-CN
-// @version      1.2
+// @version      1.3
 // @description  内嵌于审判成功提示框的本地计数器
 // @author       Eirei
 // @match        http://dnf.qq.com/cp/*
 // @match        https://dnf.qq.com/cp/*
 // @icon         https://cdn.jsdelivr.net/gh/keyonchen/script/logo.png
-// @updateURL    https://cdn.jsdelivr.net/gh/keyonchen/script@latest/judgecount.meta.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/keyonchen/script@latest/judgecount.user.js
+// @updateURL    https://cdn.jsdelivr.net/gh/keyonchen/script/judgecount.meta.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/keyonchen/script/judgecount.user.js
 // grant         none
 // ==/UserScript==
 
@@ -31,6 +31,24 @@
         style.innerHTML = css;
         head.appendChild(style);
     }
+
+    //------自定义按键列表------
+    // 标准键值参考链接
+    // https://developer.mozilla.org/zh-CN/docs/Web/API/KeyboardEvent/key/Key_Values
+    // 启用=true/禁用=false
+    const keyToggle = false;
+    // 审核结果
+    const resultKeys = ["1","2","3"];
+    // 违规类型
+    const pkcPveKeys = [
+        "q","w","e","r",
+        "a","s","d","f",
+        "z","x","c","v",
+        "Control"
+    ];
+    // 违规对象
+    const protagonistKeys = ["4","5"];
+    //------自定义按键列表------
 
     // 打印主视角玩家的角色名
     showRoleName();
@@ -59,7 +77,7 @@
         (document.head||document.documentElement).appendChild(script);
         script.remove();
     }
-  
+
     // 审判视频分类层
     const pkcPveLayerOBS = new MutationObserver(function(){
         // 隐藏红色提示栏
@@ -81,6 +99,42 @@
     const pkcPveLayer = document.getElementById("pkcPveLayer");
     const pkcPveLayerOPT = { childList : true };
     pkcPveLayerOBS.observe(pkcPveLayer,pkcPveLayerOPT);
+
+    // 映射自定义按键列表
+    if(keyToggle){
+        // 初始化
+        let keymap;
+        addKeymap(resultKeys,"#result input[type='radio']");
+        addKeymap(pkcPveKeys,"#pkvPve input[type='checkbox']");
+        addKeymap(protagonistKeys,"#protagonist input[type='radio']");
+        function addKeymap(keys,queryStr){
+            if(!keymap){
+                keymap = {};
+            }
+            for (let i = 0; i < keys.length; i++) {
+                var key = keys[i];
+                keymap[key] = {
+                    "queryStr":queryStr,
+                    "index":i
+                }
+            }
+        }
+        // 监听按键抬起事件
+        document.addEventListener("keyup", function(e){
+            // 未匹配按键列表或未提供审核项
+            if (!keymap[e.key] || pkcPveLayer.childElementCount == 0) {
+                return;
+            }
+            var key = keymap[e.key];
+            var queryStr = key.queryStr;
+            var items = pkcPveLayer.querySelectorAll(queryStr);
+            if(items.length == 0){
+                return;
+            }
+            var index = parseInt(key.index);
+            items[index].click();
+        });
+    }
 
     // 审判成功弹出层
     const popTeamOBS = new MutationObserver(function(mutations){
