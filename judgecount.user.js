442e6fec4b318855916791c9669fa4da01fea7a6
keyonchen <keyonchern@gmail.com>
2021-08-11 09:29:28 +0800

    1.5

--- judgecount.user.js
+++ judgecount.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         审判计数器
 // @namespace    https://greasyfork.org/zh-CN
-// @version      1.4
+// @version      1.5
 // @description  内嵌于审判成功提示框的本地计数器
 // @author       Eirei
 // @match        http://dnf.qq.com/cp/*
@@ -14,6 +14,32 @@
 
 (function() {
     'use strict';
+    //------自定义按键列表------
+    // 更多特殊键值参考链接
+    // https://developer.mozilla.org/zh-CN/docs/Web/API/KeyboardEvent/key/Key_Values
+    // 启用按键=true/禁用按键=false
+    const keyToggle = false;
+    // 显示键值=true/隐藏键值=false
+    const showKeyText = true;
+    // 审核结果
+    const resultKeys = ["1","2","3"];
+    // 违规类型
+    const pkcPveKeys = [
+        "q","w","e","r",
+        "a","s","d","f",
+        "z","x","c","v",
+        "Control",""
+    ];
+    // 违规对象
+    const protagonistKeys = ["4","5"];
+    // 按钮=["提交结果"]
+    const commitKey = ["`"];
+    // 按钮=["取消","继续审判","修改"]
+    const popKeys = [""," ",""];
+    //------自定义按键列表------
+
+    // 红色警示文：隐藏=true/显示=false
+    const hideContent = false;
 
     // 放大单选框
     let itemCSS = `
@@ -31,28 +57,6 @@
         style.innerHTML = css;
         head.appendChild(style);
     }
-
-    //------自定义按键列表------
-    // 标准键值参考链接
-    // https://developer.mozilla.org/zh-CN/docs/Web/API/KeyboardEvent/key/Key_Values
-    // 启用=true/禁用=false
-    const keyToggle = false;
-    // 审核结果
-    const resultKeys = ["1","2","3"];
-    // 违规类型
-    const pkcPveKeys = [
-        "q","w","e","r",
-        "a","s","d","f",
-        "z","x","c","v",
-        "Control"
-    ];
-    // 违规对象
-    const protagonistKeys = ["4","5"];
-    // 按钮=提交结果
-    const commitKey = [""];
-    // 按钮=[取消,继续审判,修改]
-    const popKeys = ["","",""];
-    //------自定义按键列表------
 
     // 打印主视角玩家的角色名
     showRoleName();
@@ -82,30 +86,8 @@
         script.remove();
     }
 
-    // 审判视频分类层
-    const pkcPveLayerOBS = new MutationObserver(function(){
-        // 隐藏红色提示栏
-        //document.getElementById("pkcPveTip").style = "display: none";
-        // 显示视频进度条
-        document.getElementById("video_container").firstElementChild.controls = "controls";
-        // 折叠判定原因框
-        var summary = document.createElement("summary");
-        summary.className = "title";
-        summary.appendChild(document.createTextNode("判定原因"));
-        var details = document.createElement("details");
-        details.appendChild(summary);
-        var trailTextarea = document.getElementById("trailTextarea");
-        var parentNode = trailTextarea.parentNode;
-        parentNode.removeChild(parentNode.firstElementChild);
-        details.appendChild(parentNode.removeChild(trailTextarea));
-        parentNode.appendChild(details);
-    });
-    const pkcPveLayer = document.getElementById("pkcPveLayer");
-    const pkcPveLayerOPT = { childList : true };
-    pkcPveLayerOBS.observe(pkcPveLayer,pkcPveLayerOPT);
-
+    // 初始化映射字典
     if(keyToggle){
-        // 初始化映射字典
         let keymap;
         addKeymap(resultKeys,"spsp1","#result input[type='radio']");
         addKeymap(pkcPveKeys,"spsp1","#pkvPve input[type='checkbox']");
@@ -154,14 +136,53 @@
         });
     }
 
-    // 审判成功弹出层
-    const popTeamOBS = new MutationObserver(function(mutations){
-        let mutation;
-        for(mutation of mutations){
-            if(mutation.target.style.display != "block"){
-                return;
+    // 审判视频分类层
+    const videoOBS = new MutationObserver(function(){
+        // 显示视频进度条
+        video.firstElementChild.controls = true;
+        // 手机端自动播放
+        video.firstElementChild.muted = true;
+        // 获取审核选项层
+        var pkcPveLayer = video.parentElement.lastElementChild;
+        // 隐藏红色警示文
+        if(hideContent){
+            pkcPveLayer.previousElementSibling.style.display = "none";
+        }
+        // 折叠判定原因框
+        var itemText = pkcPveLayer.lastElementChild.previousElementSibling;
+        var summary = document.createElement("summary");
+        summary.className = "title";
+        summary.appendChild(document.createTextNode("判定原因"));
+        var details = document.createElement("details");
+        details.appendChild(summary);
+        details.appendChild(itemText.removeChild(itemText.lastElementChild));
+        itemText.removeChild(itemText.firstElementChild);
+        itemText.appendChild(details);
+        // 显示键值文本项
+        if(keyToggle && showKeyText){
+            var keys = resultKeys.concat(pkcPveKeys,protagonistKeys);
+            var items = pkcPveLayer.querySelectorAll("li");
+            for(var i = 0 ; i < items.length; i++){
+                var item = items[i];
+                var key = keys[i];
+                if(!key || key == ""){
+                    continue;
+                }
+                var span = document.createElement("span");
+                span.appendChild(document.createTextNode(key));
+                item.appendChild(span);
             }
-        };
+        }
+    });
+    const video = document.getElementById("video_container");
+    const videoOPT = { childList : true };
+    videoOBS.observe(video,videoOPT);
+
+    // 审判成功弹出层
+    const popTeamOBS = new MutationObserver(function(){
+        if(popTeam.style.display != "block"){
+            return;
+        }
         // 累加计数
         var total_count = localStorage.getItem("TotalCount");
         if(!total_count) {
@@ -170,13 +191,13 @@
         total_count = parseInt(total_count) + 1;
         localStorage.setItem("TotalCount",total_count);
         // 响应布局
-        var input_count = document.getElementById("icount");
+        var input_count = popTeam.querySelector("#icount");
         if(input_count){
             input_count.value = total_count;
         }
         else
         {
-            mutation.target.lastElementChild.appendChild(createLayout(total_count));
+            popTeam.lastElementChild.appendChild(createLayout(total_count));
         };
     });
     const popTeam = document.getElementById("popTeam");
@@ -196,7 +217,7 @@
         button.style = "margin-left: 10px;";
         button.appendChild(btn_text);
         button.addEventListener('click',function(){
-            var current_count = parseInt(document.getElementById("icount").value);
+            var current_count = parseInt(popTeam.querySelector("#icount").value);
             if(isNaN(current_count)){
                 alert("请输入正确的数字");
                 return;
