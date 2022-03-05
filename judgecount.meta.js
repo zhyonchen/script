ebf3391064a8a57f5369c7fdaa26c47c50521e1c
zhyonchen <zhyonchen@outlook.com>
2022-03-05 12:14:09 +0800

    2.0

--- judgecount.meta.js
+++ judgecount.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         审判计数器
-// @version      1.9
+// @version      2.0
 // @description  根据版本号检查脚本更新
 // @updateURL    https://cdn.jsdelivr.net/gh/zhyonchen/script/judgecount.meta.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/zhyonchen/script/judgecount.user.js
--- judgecount.user.js
+++ judgecount.user.js
@@ -1,42 +1,45 @@
 // ==UserScript==
 // @name         审判计数器
 // @namespace    https://greasyfork.org/zh-CN
-// @version      1.9
+// @version      2.0
 // @description  内嵌于审判成功提示框的本地计数器
 // @author       Eirei
-// @match        http://dnf.qq.com/cp/*
-// @match        https://dnf.qq.com/cp/*
+// @match        http://dnf.qq.com/cp/*spxt/*
+// @match        https://dnf.qq.com/cp/*spxt/*
 // @icon         https://cdn.jsdelivr.net/gh/zhyonchen/script/logo.png
 // @updateURL    https://cdn.jsdelivr.net/gh/zhyonchen/script/judgecount.meta.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/zhyonchen/script/judgecount.user.js
-// grant         none
+// @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    //------自定义按键列表------
+    //------自定义按键列表开始------
     // 更多特殊键值参考链接
     // https://developer.mozilla.org/zh-CN/docs/Web/API/KeyboardEvent/key/Key_Values
+    // 例如："`"代表键盘左上角波浪键以及" "代表键盘上的空格键
     // 启用按键=true/禁用按键=false
     const keyToggle = false;
     // 显示键值=true/隐藏键值=false
     const showKeyText = true;
+    // 重载按钮
+    const reloadKey = ["g"];
     // 审核结果
-    const resultKeys = ["1","2","3"];
+    const resultKeys = ["1", "2", "3"];
     // 违规类型
     const pkcPveKeys = [
-        "q","w","e","r",
-        "a","s","d","f",
-        "z","x","c","v",
-        "Control",""
+        "q", "w", "e", "r",
+        "a", "s", "d", "f",
+        "z", "x", "c", "v",
+        "Control", ""
     ];
     // 违规对象
-    const protagonistKeys = ["4","5"];
+    const protagonistKeys = ["4", "5"];
     // 按钮=["提交结果"]
     const commitKey = ["Escape"];
-    // 按钮=["取消","继续审判","修改"]
-    const popKeys = ["","`",""];
-    //------自定义按键列表------
+    // 按钮=["继续审判"]
+    const popKeys = ["`"];
+    //------自定义按键列表结束------
 
     // 红色警示文：隐藏=true/显示=false
     const hideContent = false;
@@ -52,15 +55,16 @@
     `;
 
     // 初始化脚本
-    let keymap,parentElement;
+    let keymap, parentElement;
     let pkcPveTip = document.getElementById("pkcPveTip");
     let popTeam = document.getElementById("popTeam");
     InitScript();
-    function InitScript(){
+
+    function InitScript() {
         addGlobalStyle(itemCSS);
         showRoleName();
-        listenKeyUp();
         createColumnLayout();
+        listenKeyUp();
     };
 
     // 添加自定义样式表
@@ -75,62 +79,63 @@
     }
 
     // 捕获控制台信息
-    function showRoleName(){
+    function showRoleName() {
         var actualCode = '(' + function() {
             var old_console_log = window.console.log;
-            window.console.log = function(msg){
+            window.console.log = function(msg) {
                 old_console_log(msg);
-                if(typeof(msg) == "object"){
-                    if(msg.hasOwnProperty("jData")){
+                if (typeof(msg) == "object") {
+                    if (msg.hasOwnProperty("jData")) {
                         // 记录审判入口
                         var access_id = msg.jData.data.access_id;
-                        if(localStorage.getItem("access_id") != access_id){
-                            localStorage.setItem("access_id",access_id);
+                        if (localStorage.getItem("access_id") != access_id) {
+                            localStorage.setItem("access_id", access_id);
                         }
                         // 标记PVE中的PKC
                         var pattern_type = msg.jData.data.case_info.pattern_type_desc;
-                        if(localStorage.getItem("pattern_type") != pattern_type){
-                            localStorage.setItem("pattern_type",pattern_type);
+                        if (localStorage.getItem("pattern_type") != pattern_type) {
+                            localStorage.setItem("pattern_type", pattern_type);
                         }
                         // 记录角色名
-                        localStorage.setItem("role_name",msg.jData.data.defendant_role_name);
+                        localStorage.setItem("role_name", msg.jData.data.defendant_role_name);
                     }
                 };
             };
         } + ')();';
         var script = document.createElement('script');
         script.textContent = actualCode;
-        (document.head||document.documentElement).appendChild(script);
+        (document.head || document.documentElement).appendChild(script);
         script.remove();
     }
 
     // 监听按键抬起
-    function listenKeyUp(){
-        if(!keyToggle){
+    function listenKeyUp() {
+        if (!keyToggle) {
             return;
         }
-        addKeymap(resultKeys,"spsp1","#result input[type='radio']");
-        addKeymap(pkcPveKeys,"spsp1","#pkvPve input[type='checkbox']");
-        addKeymap(protagonistKeys,"spsp1","#protagonist input[type='radio']");
-        addKeymap(commitKey,"spsp1","#judgeCommit");
-        addKeymap(popKeys,"popTeam","button");
-        document.addEventListener("keyup", function(e){
+        addKeymap(reloadKey, "video_container", "#reload");
+        addKeymap(resultKeys, "spsp1", "#result input[type='radio']");
+        addKeymap(pkcPveKeys, "spsp1", "#pkvPve input[type='checkbox']");
+        addKeymap(protagonistKeys, "spsp1", "#protagonist input[type='radio']");
+        addKeymap(commitKey, "spsp1", "#judgeCommit");
+        addKeymap(popKeys, "popTeam", "#judgeNext");
+        document.addEventListener("keyup", function(e) {
             // 未匹配按键
             var key = keymap[e.key];
             if (!key) {
                 return;
             }
             // 获取当前父元素
-            if(!parentElement || parentElement.id != key.parentID){
+            if (!parentElement || parentElement.id != key.parentID) {
                 parentElement = document.getElementById(key.parentID);
             }
             // 父元素处于隐藏状态
-            if(parentElement.style.display == "none"){
+            if (parentElement.style.display == "none") {
                 return;
             }
             // 查询子元素
             var items = parentElement.querySelectorAll(key.queryStr);
-            if(items.length == 0){
+            if (items.length == 0) {
                 return;
             }
             var index = parseInt(key.index);
@@ -138,39 +143,46 @@
         });
 
     }
-    function addKeymap(keys,parentID,queryStr){
-        if(!keymap){
+
+    function addKeymap(keys, parentID, queryStr) {
+        if (!keymap) {
             keymap = {};
         }
         for (let i = 0; i < keys.length; i++) {
             var key = keys[i];
-            if(key == ""){
+            if (key == "") {
                 continue;
             }
             keymap[key] = {
-                "parentID":parentID,
-                "queryStr":queryStr,
-                "index":i
+                "parentID": parentID,
+                "queryStr": queryStr,
+                "index": i
             }
         }
     }
 
     // 计数器分列布局
-    function createColumnLayout(){
-        var pveInput = createInput("pveCount","pveInput","margin-left: 1px;",1);
-        var pkcInput = createInput("pkcCount","pkcInput","margin-left: 1px;",1);
+    function createColumnLayout() {
+        // 标记继续审判按钮
+        var judgeNext = popTeam.querySelectorAll(".tac button")[1];
+        if (judgeNext) {
+            judgeNext.id = "judgeNext";
+        }
+        // 分列布局
+        var pveInput = createInput("pveCount", "pveInput", "margin-left: 1px;", 1);
+        var pkcInput = createInput("pkcCount", "pkcInput", "margin-left: 1px;", 1);
         var button = document.createElement("button");
         var btn_text = document.createTextNode("修改");
         button.appendChild(btn_text);
-        button.addEventListener('click',function(){
+        button.addEventListener('click', function() {
             var pveCount = parseInt(popTeam.querySelector("#pveInput").value);
             var pkcCount = parseInt(popTeam.querySelector("#pkcInput").value);
-            if(isNaN(pveCount)||isNaN(pkcCount)){
+            if (isNaN(pveCount) || isNaN(pkcCount)) {
                 alert("请输入正确的数字");
                 return;
             }
-            localStorage.setItem("pveCount",pveCount);
-            localStorage.setItem("pkcCount",pkcCount);
+            localStorage.setItem("pveCount", pveCount);
+            localStorage.setItem("pkcCount", pkcCount);
             alert("修改成功");
         });
         //创建表格
@@ -206,7 +218,7 @@
         popTeam.lastElementChild.appendChild(table);
     }
 
-    function createInput(storageId,elementId,style,size){
+    function createInput(storageId, elementId, style, size) {
         var input = document.createElement("input");
         input.id = elementId;
         input.className = "txc";
@@ -217,20 +229,39 @@
     }
 
     // 审判视频分类层
-    const pkcPveTipOBS = new MutationObserver(function(){
+    const pkcPveTipOBS = new MutationObserver(function() {
         // 显示视频进度条
         var video = pkcPveTip.parentElement.firstElementChild;
+        video.firstElementChild.id = "video"
         video.firstElementChild.controls = true;
+        // 禁止Safari全屏
+        video.firstElementChild.setAttribute("playsinline","");
         // 视频静音播放
         video.firstElementChild.muted = videoMuted;
         // 嵌入玩家角色名
-        video.insertBefore(createRoleNameText(),video.firstElementChild);
-        video.appendChild(createRoleNameText());
+        video.insertBefore(createRoleNameText(), video.firstElementChild);
+        // 视频重载按钮
+        video.appendChild(createReloadButton());
         // 获取审核选项层
         var pkcPveLayer = video.parentElement.lastElementChild;
         // 隐藏红色警示文
-        if(hideContent){
+        if (hideContent) {
             pkcPveLayer.previousElementSibling.style.display = "none";
+        }
+        // 显示键值文本项
+        if (keyToggle && showKeyText) {
+            var keys = resultKeys.concat(pkcPveKeys, protagonistKeys);
+            var items = pkcPveLayer.querySelectorAll("li");
+            for (var i = 0; i < items.length; i++) {
+                var item = items[i];
+                var key = keys[i];
+                if (!key || key == "") {
+                    continue;
+                }
+                var span = document.createElement("span");
+                span.appendChild(document.createTextNode(key));
+                item.appendChild(span);
+            }
         }
         // 折叠判定原因框
         var itemText = pkcPveLayer.lastElementChild.previousElementSibling;
@@ -242,59 +273,80 @@
         details.appendChild(itemText.removeChild(itemText.lastElementChild));
         itemText.removeChild(itemText.firstElementChild);
         itemText.appendChild(details);
-        // 显示键值文本项
-        if(keyToggle && showKeyText){
-            var keys = resultKeys.concat(pkcPveKeys,protagonistKeys);
-            var items = pkcPveLayer.querySelectorAll("li");
-            for(var i = 0 ; i < items.length; i++){
-                var item = items[i];
-                var key = keys[i];
-                if(!key || key == ""){
-                    continue;
-                }
-                var span = document.createElement("span");
-                span.appendChild(document.createTextNode(key));
-                item.appendChild(span);
+        // 记录审查结果
+        var commitButton = pkcPveLayer.lastElementChild;
+        commitButton.addEventListener('click', function() {
+            var resultItem = document.getElementById("result");
+            var input = resultItem.querySelector("input[type='radio']:checked");
+            if (!input) {
+                return;
             }
-        }
+            //  违规类型
+            var type = "";
+            switch (input.value) {
+                case "0":
+                    type = "无违规";
+                    break;
+                case "1":
+                    type = "违规";
+                    break;
+                case "2":
+                    type = "不确定";
+                    break;
+            }
+
+        });
+
     });
-    function createRoleNameText(){
+    // 角色名文本
+    function createRoleNameText() {
         var text = document.createElement("p");
         text.className = "p2";
         text.style = "position:absolute;"
         text.innerHTML = localStorage.getItem("role_name");
         return text;
     }
+    // 重载按钮
+    function createReloadButton(text) {
+        var div = document.createElement("div");
+        div.id = "reload";
+        div.appendChild(createRoleNameText());
+        div.addEventListener('click', function() {
+            document.getElementById("video").load();
+        });
+        return div;
+    }
 
     // 审判成功弹出层
-    const popTeamOBS = new MutationObserver(function(){
-        if(popTeam.style.display != "block"){
+    const popTeamOBS = new MutationObserver(function() {
+        if (popTeam.style.display != "block") {
             return;
         }
         var access_id = localStorage.getItem("access_id");
-        if(access_id == "0_1_5"){
-            updateCount("pkcCount","pkcInput");
+        if (access_id == "0_1_5") {
+            updateCount("pkcCount", "pkcInput");
         }
-        if(access_id == "0_1_6"){
+        if (access_id == "0_1_6") {
             var pattern_type = localStorage.getItem("pattern_type");
-            if( pattern_type == 443 ){
-                updateCount("pkcCount","pkcInput");
+            if (pattern_type == 443) {
+                updateCount("pkcCount", "pkcInput");
                 return;
             }
-            updateCount("pveCount","pveInput");
+            updateCount("pveCount", "pveInput");
         }
     });
-    function updateCount(storageId,elementId){
+
+    function updateCount(storageId, elementId) {
         var count = localStorage.getItem(storageId);
-        if(!count){
+        if (!count) {
             count = 0;
         }
         count = parseInt(count) + 1;
-        localStorage.setItem(storageId,count);
-        popTeam.querySelector("#"+elementId).value = count;
+        localStorage.setItem(storageId, count);
+        popTeam.querySelector("#" + elementId).value = count;
     }
     // 派遣观察者
-    pkcPveTipOBS.observe(pkcPveTip, { childList : true });
-    popTeamOBS.observe(popTeam, { attributeFilter:["style"] });
+    pkcPveTipOBS.observe(pkcPveTip, { childList: true });
+    popTeamOBS.observe(popTeam, { attributeFilter: ["style"] });
 
 })();
