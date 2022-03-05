51ae0537ad5c960870f63617a0520400505bc27c
zhyonchen <zhyonchen@outlook.com>
2022-03-05 12:21:12 +0800

    2.2

--- judgecount.meta.js
+++ judgecount.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         审判计数器
-// @version      2.1
+// @version      2.2
 // @description  根据版本号检查脚本更新
 // @updateURL    https://cdn.jsdelivr.net/gh/zhyonchen/script/judgecount.meta.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/zhyonchen/script/judgecount.user.js
--- judgecount.user.js
+++ judgecount.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         审判计数器
 // @namespace    https://greasyfork.org/zh-CN
-// @version      2.1
+// @version      2.2
 // @description  内嵌于审判成功提示框的本地计数器
 // @author       Eirei
 // @match        http://dnf.qq.com/cp/*spxt/*
@@ -15,31 +15,30 @@
 (function() {
     'use strict';
 
-    const path = "https://cdn.jsdelivr.net/gh/zhyonchen/script";
+    const path = "https://cdn.jsdelivr.net/gh/zhyonchen/script/";
     const defaultSettingFile = "defaultSetting.json";
     const myStyleFile = "myStyle.css";
+    const anchorJson = { "setting": "设置" };
 
     let keymap, parentElement;
     let logined = document.getElementById("logined");
     let popTeam = document.getElementById("popTeam");
-    let isLocked = false;
 
     // 程序主入口
     InitScript();
 
     function InitScript() {
         loadResource();
+        updateKeymap();
         listenKeyUp();
         registerHook();
-        createSettingAnchor();
-        createSettingLayout();
+        createAnchor();
         createColumnLayout();
     }
 
     async function loadResource() {
-        const defaultSettingFilePromise = await getStaticFile(defaultSettingFile, 'json');
-        const myStyleFilePromise = await getStaticFile(myStyleFile, 'text');
-        // 等待执行结果返回
+        const defaultSettingFilePromise = getStaticFile(defaultSettingFile, 'json');
+        const myStyleFilePromise = getStaticFile(myStyleFile, 'text');
         let values = await Promise.all([defaultSettingFilePromise, myStyleFilePromise]);
         if (values.length > 0) {
             let defaultSetting = values[0];
@@ -49,13 +48,11 @@
             // 注入自定义CSS
             addGlobalStyle("myStyle", myStyle);
         }
-        // 更新按键映射
-        updateKeymap(await getLocalSetting());
     }
 
     async function getStaticFile(filename, fileType) {
-        let url = path + '/' + filename;
-        let response = await fetch(url);
+        let url = path + filename;
+        let response = await fetch(url, { cache: "no-cache" });
         if (response.ok) {
             let content;
             switch (fileType) {
@@ -68,7 +65,7 @@
             }
             return content;
         } else {
-            window.console.log(`HTTP error! status: ${response.status} fileType: ${filename}`);
+            window.console.error(`HTTP error! status: ${response.status} file: ${filename}`);
         }
     }
 
@@ -76,22 +73,22 @@
         let setting = JSON.parse(localStorage.getItem("setting"));
         if (!setting) {
             if (!defaultSetting) {
-                alert("初始化配置失败，请刷新页面");
+                alert("无法加载配置文件，审判计数器部分功能失效");
                 return;
             }
-            localStorage.setItem("setting", JSON.stringify(defaultSetting));
+            // 本地配置不存在，但默认配置存在，使用默认配置初始化本地配置
+            updateLocalSetting(defaultSetting);
         } else if (defaultSetting && setting.fileVersion < defaultSetting.fileVersion) {
-            // 用本地配置替换默认配置
-            for (var key in defaultSetting) {
+            for (let key in defaultSetting) {
                 if (key == "fileVersion") {
                     continue;
                 }
-                if (setting.key) {
+                if (setting[key]) {
                     defaultSetting[key] = setting[key];
                 }
             }
-            // 更新本地配置
-            localStorage.setItem("setting", JSON.stringify(defaultSetting));
+            // 本地配置和默认配置存在，且版本号不一致，使用本地配置替换默认配置
+            updateLocalSetting(defaultSetting);
         }
     }
 
@@ -105,10 +102,46 @@
         style.innerHTML = css;
         head.appendChild(style);
     }
+    // 加载配置
+    async function getLocalSetting() {
+        let n = 30;
+        while (!localStorage.setting) {
+            if (n <= 0) {
+                return null;
+            }
+            await sleep(100);
+            n--;
+        }
+        return JSON.parse(localStorage.setting);
+    }
+    // 更新配置
+    function updateLocalSetting(newSetting) {
+        let oldSetting = localStorage.setting;
+        localStorage.setting = JSON.stringify(newSetting);
+        updateKeymap();
+        let spsp1 = document.getElementById('spsp1');
+        if (spsp1.style.display == 'none') {
+            return;
+        }
+        if(!oldSetting){
+            return;
+        }
+        oldSetting = JSON.parse(oldSetting);
+        if (oldSetting.oldPlayer&&oldSetting.oldPlayer.data == newSetting.oldPlayer.data) {
+            updatePkcPveLayout();
+            return;
+        } 
+        continueJudge();
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
     // 键值映射
-    function updateKeymap(setting) {
+    async function updateKeymap() {
         keymap = {};
+        let setting = await getLocalSetting();
         if (!setting || !setting.keyToggle.data) {
             return;
         }
@@ -128,9 +161,6 @@
                     "index": i
                 }
             }
-        }
-        if (Object.keys(keymap).length == 0) {
-            alert("按键功能失效，请刷新页面");
         }
     }
 
@@ -162,108 +192,107 @@
         });
     }
 
-    async function getLocalSetting() {
-        // 尝试直接加载
-        let setting = JSON.parse(localStorage.getItem("setting"));
-        if (!setting) {
-            // 等待再次加载
-            await sleep(100);
-            setting = JSON.parse(localStorage.getItem("setting"));
-        }
-        return setting;
-    }
-
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
     function registerHook() {
-        window.addEventListener('load', function() {
+        window.addEventListener('load', () => {
             let getJudgeCase = amsCfg_628988.fFlowSubmitEnd;
             amsCfg_628988.fFlowSubmitEnd = function(res) {
-                getJudgeCase(res);
-                if (res.iRet == "0") {
-                    localStorage.setItem("data", JSON.stringify(res.jData.data));
-                    updateVideoLayout();
-                    updatePkcPveLayout();
-                }
+                getLocalSetting().then((setting) => {
+                    if (res.iRet == 0) {
+                        localStorage.data = JSON.stringify(res.jData.data);
+                        if (setting && setting.oldPlayer && setting.oldPlayer.data) {
+                            res.jData.data.play_type = 0;
+                        }
+                    }
+                    getJudgeCase(res);
+                    if (res.iRet == 0) {
+                        if (setting && setting.oldPlayer && setting.oldPlayer.data) {
+                            updateOldPlayerLayout();
+                        } else {
+                            updateTCPlayerLayout();
+                        }
+                        updatePkcPveLayout();
+                    }
+                });
             };
-
             let getJudgeResult = amsCfg_628989.fFlowSubmitEnd;
             amsCfg_628989.fFlowSubmitEnd = function(res) {
-                let data = JSON.parse(localStorage.getItem("data"));
+                let data = JSON.parse(localStorage.data);
                 if (!data) {
                     alert("计数失败");
-                } else if (data.access_id == "0_1_6") {
-                    if (data.case_info.pattern_type_desc == 443) {
-                        updateCount("pkcCount");
-                    } else {
-                        updateCount("pveCount");
-                    }
-                } else if (data.access_id == "0_1_5") {
+                } else if (data.access_id == "0_0_2") {
+                    updateCount("pveCount");
+                } else if (data.access_id == "0_0_1") {
                     updateCount("pkcCount");
                 }
                 getJudgeResult(res);
             };
-        });
+        }, { once: true });
     }
 
-    function createSettingAnchor() {
-        let settingAnchor = document.createElement('a');
-        settingAnchor.href = "javascript:void(0);";
-        settingAnchor.textContent = "设置"
-        settingAnchor.addEventListener('click', onSettingAnchorClick);
-        logined.appendChild(settingAnchor);
+    function createAnchor() {
+        for (let key in anchorJson) {
+            let anchor = document.createElement('a');
+            anchor.id = key;
+            anchor.href = "javascript:void(0);";
+            anchor.textContent = anchorJson[key];
+            anchor.addEventListener('click', callDialog);
+            logined.appendChild(anchor);
+        }
     }
 
-    function onSettingAnchorClick() {
-        let dialog = document.getElementById("settingLayout");
+    async function callDialog(e) {
+        let dialog = logined.querySelector('dialog');
         if (!dialog) {
-            alert("设置面板正在创建中……");
-            return;
+            let form = document.createElement('form');
+            form.method = 'dialog';
+            dialog = document.createElement('dialog');
+            dialog.appendChild(form);
+            logined.appendChild(dialog);
+            dialog.addEventListener('cancel', clearForm);
         }
         if (typeof dialog.showModal != 'function') {
             alert("The dialog API is not supported by this browser");
             return;
         }
-        if (!isLocked) {
-            isLocked = true;
-            getLocalSetting()
-                .then((setting) => {
-                    updateSettingForm(setting);
-                    if (dialog.querySelector('input')) {
-                        dialog.showModal();
-                    };
-                    isLocked = false;
-                })
-                .catch(e => alert(e));
-        } else {
-            alert("设置面板正在创建中……");
+        let text = e.target.textContent;
+        let form = dialog.querySelector('form');
+        callMaskDialog(true);
+        switch (text) {
+            case "设置":
+                await updateSettingForm(form);
+                break;
         }
-    }
-
-    function createSettingLayout() {
-        // 对话框
-        let dialog = document.createElement("dialog");
-        dialog.id = "settingLayout";
-        dialog.addEventListener('cancel', clearSettingForm);
-        // 表单
-        let form = document.createElement('form');
-        form.id = "settingForm";
-        form.method = "dialog";
-        // 创建布局
-        dialog.appendChild(form);
-        logined.appendChild(dialog);
-    }
-
-    function updateSettingForm(setting) {
-        if (!setting) {
-            alert("加载配置失败，无法创建设置面板");
+        callMaskDialog(false);
+        if (form.innerHTML == "") {
+            alert("面板创建失败，请重试");
             return;
         }
-        let form = logined.querySelector('#settingForm');
+        dialog.showModal();
+    }
+
+    function clearForm() {
+        let dialog = logined.querySelector('dialog');
+        dialog.querySelector('form').innerHTML = "";
+        dialog.close();
+    }
+
+    function callMaskDialog(toggle) {
+        let popupMsg = document.getElementById("_PopupMsg_");
+        let overlay = document.getElementById("_overlay_");
+        if (popupMsg) {
+            popupMsg.style.display = toggle ? "block" : "none";
+        }
+        if (overlay) {
+            overlay.style.display = toggle ? "block" : "none";
+        }
+    }
+
+    async function updateSettingForm(form) {
         if (!form) {
-            alert("页面已过期，请刷新页面");
+            return;
+        }
+        let setting = await getLocalSetting();
+        if (setting == null) {
             return;
         }
         // 排序
@@ -315,7 +344,7 @@
         div.className = "tip-con";
         div.appendChild(createFormButton("重设", onResetButtonClick));
         div.appendChild(createFormButton("保存", onSaveButtonClick));
-        div.appendChild(createFormButton("取消", clearSettingForm));
+        div.appendChild(createFormButton("取消", clearForm));
         button_li.appendChild(div);
         ul.appendChild(button_li);
         form.appendChild(ul);
@@ -328,29 +357,31 @@
         return button;
     }
 
-    function onResetButtonClick() {
-        let currentSetting = localStorage.getItem("setting");
-        localStorage.setItem("setting", null);
-        clearSettingForm();
-        loadResource().then(() => {
-            getLocalSetting().then((setting) => {
-                updateSettingForm(setting);
-                alert("重设成功，请重新保存");
-                logined.querySelector('#settingLayout').showModal();
-            });
-        }).catch((e) => {
-            localStorage.setItem("setting", currentSetting);
-            alert("重设失败,本地配置未发生改变");
-        })
+    async function onResetButtonClick() {
+        let oldSetting = localStorage.setting;
+        localStorage.removeItem("setting");
+        await loadResource();
+        if (!localStorage.setting) {
+            alert("重设失败，还原先前配置");
+            localStorage.setting = oldSetting;
+            logined.querySelector("#setting").click();
+        } else {
+            alert("重设成功,已初始化本地配置");
+            clearForm();
+        }
     }
 
-    async function onSaveButtonClick() {
+    async function onSaveButtonClick(e) {
         let setting = await getLocalSetting();
         if (!setting) {
             alert("保存失败");
             return;
         }
-        let form = document.getElementById('settingForm');
+        let form = getParentByTag(e.target, 'FORM');
+        if (!form) {
+            alert("保存失败");
+            return;
+        }
         let inputs = form.querySelectorAll('input');
         for (let i = 0; i < inputs.length; i++) {
             let input = inputs[i];
@@ -372,40 +403,82 @@
                 setting[key] = item;
             }
         }
-        localStorage.setItem("setting", JSON.stringify(setting));
-        updateKeymap(setting);
-        clearSettingForm();
-        updatePkcPveLayout();
+        await updateLocalSetting(setting);
+        clearForm();
         alert("保存成功");
     }
 
-    function clearSettingForm() {
-        document.getElementById('settingLayout').close();
-        document.getElementById('settingForm').innerHTML = ""
+    function getParentByTag(element, tag) {
+        while (element && element.tagName != tag) {
+            element = element.parentElement;
+        }
+        return element;
     }
 
-    // 更新视频层
-    function updateVideoLayout() {
-        let setting = JSON.parse(localStorage.getItem("setting"));
+    // 更新老播放器
+    async function updateOldPlayerLayout() {
         let video_container = document.getElementById("video_container");
         let video = video_container.querySelector("video");
-        video.id = "video";
+        video.id = "oldPlayer";
         video.controls = true;
         // 禁止Safari全屏
         video.setAttribute("playsinline", "");
         // 视频静音播放
-        video.muted = setting.videoMuted.data;
+        video.muted = true;
         // 嵌入玩家角色名
         video_container.insertBefore(createRoleNameText(), video);
-        // 视频重载按钮
-        video_container.appendChild(createReloadButton());
+        video_container.appendChild(createReloadText());
+    }
+    // 更新TC播放器
+    async function updateTCPlayerLayout() {
+        let video_container = document.getElementById("cloud_video_container");
+        let textArray = video_container.querySelectorAll('.p2');
+        if (textArray.length == 0) {
+            let player = video_container.querySelector("#cloud-player");
+            player.appendChild(createRoleNameText());
+            video_container.appendChild(createReloadText());
+            return;
+        }
+        let roleName = JSON.parse(localStorage.getItem("data")).defendant_role_name;
+        for (let text of textArray) {
+            text.innerHTML = roleName;
+        }
     }
 
-    // 更新分类层
-    function updatePkcPveLayout() {
-        let setting = JSON.parse(localStorage.getItem("setting"));
+    // 角色名文本
+    function createRoleNameText() {
+        var text = document.createElement("p");
+        text.className = "p2";
+        text.style = "position:absolute;"
+        text.innerHTML = JSON.parse(localStorage.getItem("data")).defendant_role_name;
+        return text;
+    }
+
+    // 重载视频
+    function createReloadText() {
+        let text = createRoleNameText();
+        text.id = "reload";
+        text.addEventListener('click', () => {
+            let tcPlayer = document.getElementById("cloud-player_html5_api");
+            let oldPlayer = document.getElementById("oldPlayer");
+            if (tcPlayer) {
+                tcPlayer.load();
+            }
+            if (oldPlayer) {
+                oldPlayer.load();
+            }
+        })
+        return text;
+    }
+
+    // 更新审判分类面板
+    async function updatePkcPveLayout() {
         let pkcPveLayer = document.getElementById("pkcPveLayer");
         if (pkcPveLayer.children.length == 0) {
+            return;
+        }
+        let setting = await getLocalSetting();
+        if (!setting) {
             return;
         }
         // 隐藏红色警告
@@ -422,7 +495,7 @@
             let item = items[i];
             let span = item.querySelector(`span`);
             let word = span.innerHTML.split(' ');
-            if (setting.keyToggle.data) {
+            if (setting.showKeyText.data) {
                 let key = keys[i];
                 if (!key || key == "") {
                     continue;
@@ -443,24 +516,6 @@
             itemText.innerHTML = defaultTextareaHTML;
         }
     }
-    // 角色名文本
-    function createRoleNameText() {
-        var text = document.createElement("p");
-        text.className = "p2";
-        text.style = "position:absolute;"
-        text.innerHTML = JSON.parse(localStorage.getItem("data")).defendant_role_name;
-        return text;
-    }
-    // 重载按钮
-    function createReloadButton(text) {
-        var div = document.createElement("div");
-        div.id = "reload";
-        div.appendChild(createRoleNameText());
-        div.addEventListener('click', function() {
-            document.getElementById("video").load();
-        });
-        return div;
-    }
 
     // 计数器分列布局
     function createColumnLayout() {
@@ -468,6 +523,7 @@
         let judgeNext = popTeam.querySelectorAll(".tac button")[1];
         if (judgeNext) {
             judgeNext.id = "judgeNext";
+            judgeNext.setAttribute('onclick', "javascript:amsSubmit(135125, 628988);")
         }
         // 输入框
         let pveInput = createPkcPveInput("pveCount");
